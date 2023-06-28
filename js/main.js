2d746024e012785f57843ab2363a40f180bb4f69
Abolfazl-sli <99794161+Abolfazl-sli@users.noreply.github.com>
2023-06-28 18:01:03 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@ window.addEventListener("scroll" , (e)=>{
 })
 
 
+
+function stopWheelZoom(event) {
+    if (event.ctrlKey == true) {
+      event.preventDefault();
+    }
+  }
+  function stopKeyZoom(event) {
+    if (event.ctrlKey && [48,61,96,107,109,187,189].indexOf(event.keyCode) > -1) {
+      event.preventDefault();
+    }
+  }
+  document.addEventListener("keydown", stopKeyZoom);
+  document.addEventListener('mousewheel', stopWheelZoom);
+  document.addEventListener('DOMMouseScroll', stopWheelZoom);
